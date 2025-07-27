95a5747c6f7a470912eda3e15e4afa1ae103dafb
Muhammad Abdussalam <masalami042@gmail.com>
2025-07-28 00:35:48 +0100

    note

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -6,7 +6,7 @@ const route = useRoute()
 const event = ref(null)
 const allEvents = UniVents
 console.log(allEvents)
-
+//note
 onMounted(() => {
   const id = Number(route.params.id)
   event.value = allEvents.find((e) => e.id === id)
